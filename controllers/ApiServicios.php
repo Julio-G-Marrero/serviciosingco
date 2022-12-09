51867031227a6f107fc90fa94e8e766ffee82164
Julio-G-Marrero <58669766+Julio-G-Marrero@users.noreply.github.com>
2022-12-09 11:25:09 -0600

    Update ApiServicios.php

--- controllers/ApiServicios.php
+++ controllers/ApiServicios.php
@@ -8,11 +8,13 @@ use MVC\Router;
 
 class ApiServicios {
     public static function index(Router $router){
+        header('Access-Control-Allow-Origin: *');
         $prouectos = Proyecto::all();
         echo json_encode($prouectos);
 
     }
     public static function proyecto(Router $router){
+        header('Access-Control-Allow-Origin: *');
         $id = $_POST['id'];
         $prouectos = Proyecto::where('id',$id);
         echo json_encode($prouectos);
